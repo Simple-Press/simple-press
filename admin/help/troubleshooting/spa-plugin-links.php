1c98becf8d0446319124b3526825a0b93376db5f
Nigel Bahadur <elindydotcom@hotmail.com>
2019-03-23 08:45:07 -0500

    Committing minor change because prior commit had the wrong fix id - should be 36.  So marking this one as Fixes #36.

--- admin/help/troubleshooting/spa-plugin-links.php
+++ admin/help/troubleshooting/spa-plugin-links.php
@@ -5,7 +5,7 @@
 	<div class="clearboth"></div>
 
 	<p>Please visit our <a href="<?php echo(SPHOMESITE); ?>/store/add-ons/">Plugin Library</a></p>
-	<p>With very nearly 70 plugins available there is something every forum administrator needs in our plugin library
-	from Subscriptions to Post Editors, Private Messaging to Polls, File and Image Uploading to enhanced Search.</p>
+	<p>With nearly 70 plugins available there is something every forum administrator needs in our plugin library.
+	From Subscriptions to Post Editors, Private Messaging to Polls, File and Image Uploading to enhanced Search and more!</p>
 
 </div>
