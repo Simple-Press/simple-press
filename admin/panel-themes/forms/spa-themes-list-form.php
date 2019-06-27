d48c75e6727140b3f4cb721f2eda2193e62ea4d0
Maxim Goltsov <maxi2997@yandex.ru>
2019-06-27 23:11:08 +1000

    housekepping page

--- admin/panel-themes/forms/spa-themes-list-form.php
+++ admin/panel-themes/forms/spa-themes-list-form.php
@@ -19,7 +19,7 @@ function spa_themes_list_form() {
 	$xml = sp_load_version_xml();
 
 	spa_paint_options_init();
-	spa_paint_open_tab(SP()->primitives->admin_text('Available Themes').'- '.SP()->primitives->admin_text('Select Simple:Press Theme'), true);
+	spa_paint_open_tab(SP()->primitives->admin_text('Available Themes').' - '.SP()->primitives->admin_text('Select Simple:Press Theme'), true);
 	spa_paint_open_panel();
 
 	spa_paint_spacer();
@@ -57,7 +57,7 @@ function spa_themes_list_form() {
                 }
             }
 ?>
-    		<p class="sf-description" style="padding: 0;">
+    		<p class="sf-description" style="">
     			<?php echo $themes[$curTheme['theme']]['Description']; ?>
     		</p>
 <?php
@@ -162,7 +162,7 @@ function spa_themes_list_form() {
 	    				if ($themes[$curTheme['theme']]['Name'] == $latest->name) {
 	    					if ((version_compare($latest->version, $themes[$curTheme['theme']]['Version'], '>') == 1)) {
 	    						echo '<br />';
-	    						echo '<p style="padding: 0;">';
+	    						echo '<p style="">';
 	    						echo '<strong>'.SP()->primitives->admin_text('There is an update for the').' '.$themes[$curTheme['theme']]['Name'].' '.SP()->primitives->admin_text('theme').'.</strong> ';
 	    						echo SP()->primitives->admin_text('Version').' '.$latest->version.' '.SP()->primitives->admin_text('is available').'. ';
 	    						echo SP()->primitives->admin_text('For details and to download please visit').' '.SPPLUGHOME.' '.SP()->primitives->admin_text('or').' '.SP()->primitives->admin_text('go to the').' ';
@@ -190,6 +190,161 @@ function spa_themes_list_form() {
 ?>
 		<div class="theme-browser rendered">
 			<div class="spThemeContainer">
+			<div id="current-theme" class="spTheme">
+<?php
+        if (file_exists(SPTHEMEBASEDIR.$curTheme['theme'].'/styles/'.$curTheme['style'])) {
+?>
+			<h3 class="theme-name"><?php echo $themes[$curTheme['theme']]['Name']; ?></h3>
+
+    		<img src="<?php echo SPTHEMEBASEURL.$curTheme['theme'].'/'.$themes[$curTheme['theme']]['Screenshot']; ?>" alt="" />
+    		<h4>
+    			<?php echo $themes[$curTheme['theme']]['Name'].' '.$themes[$curTheme['theme']]['Version'].'<br />'.SP()->primitives->admin_text('by').' <a href="'.$themes[$curTheme['theme']]['AuthorURI'].'" title="'.SP()->primitives->admin_text('Visit author homepage').'">'.$themes[$curTheme['theme']]['Author'].'</a>'; ?>
+    		</h4>
+<?php
+            if (!empty($curTheme['parent'])) {
+                if (file_exists(SPTHEMEBASEDIR.$curTheme['parent'])) {
+                    echo '<p class="theme-parent">';
+                    echo SP()->primitives->admin_text('This theme is a child theme of ').'<b>'.$curTheme['parent'].'</b>';
+                    echo '</p>';
+                } else {
+                    echo '<p class="theme-parent">';
+                    echo '<b>'.SP()->primitives->admin_text('The specified parent theme')." '".$curTheme['parent']."' ".SP()->primitives->admin_text('does not exist').'</b> ';
+                    echo '</p>';
+                }
+            }
+?>
+    		<p class="sf-description" style="">
+    			<?php echo $themes[$curTheme['theme']]['Description']; ?>
+				</p><br/>
+				<div class="action-links">
+				
+<?php
+            $overlays = SP()->theme->get_overlays(SPTHEMEBASEDIR.$curTheme['theme'].'/styles/overlays');
+
+            # pull in parent overlays if child theme
+            if (!empty($curTheme['parent'])) {
+                $parent_overlays = SP()->theme->get_overlays(SPTHEMEBASEDIR.$curTheme['parent'].'/styles/overlays');
+                $overlays = array_merge($overlays, $parent_overlays);
+                $overlays = array_unique($overlays);
+            }
+
+            if (!empty($overlays)) {
+?>
+                <script>
+                        spj.loadAjaxForm('sftheme-<?php echo esc_js($curTheme['theme']); ?>', 'sfreloadtlist');
+                </script>
+                <br />
+<?php
+                $ajaxURL = wp_nonce_url(SPAJAXURL.'themes-loader&amp;saveform=theme', 'themes-loader');
+            	echo '<form action="'.$ajaxURL.'" method="post" id="sftheme-'.esc_attr($curTheme['theme']).'" name="sftheme-'.esc_attr($curTheme['theme']).'">';
+                echo sp_create_nonce('forum-adminform_themes');
+                echo '<input type="hidden" name="theme" value="'.esc_attr($curTheme['theme']).'" />';
+                echo '<input type="hidden" name="style" value="'.esc_attr($themes[$curTheme['theme']]['Stylesheet']).'" />';
+                echo '<input type="hidden" name="parent" value="'.esc_attr($curTheme['parent']).'" />';
+
+            	echo '<input type="hidden" name="default-color" value="'.esc_attr($overlays[0]).'" />';
+?>
+
+								<div class="currentTheme">
+									<span>ok</span>
+									<span>Current Theme</span>
+								</div>
+<?php
+
+				# if only one overlay hide select controls
+				$style = (count($overlays) > 1) ? 'style="display:block"' : 'style="display:none"';
+				echo '<div '.$style.'>';
+    			echo SP()->primitives->admin_text('Select Overlay').': ';
+    			echo '<select name="color-'.esc_attr($curTheme['theme']).'">';
+            	foreach ($overlays as $overlay) {
+            		$overlay = trim($overlay);
+    	    		$selected = ($curTheme['color'] == $overlay) ? ' selected="selected" ' : '';
+    				echo '<option'.$selected.' value="'.esc_attr($overlay).'">'.esc_html($overlay).'</option>';
+        		}
+    			echo '</select> ';
+								echo ' <input type="submit" class="sf-button-secondary action" id="update" name="update" value="'.SP()->primitives->admin_text('Update Overlay').'" />';
+								echo '</form>';
+					echo '</div>';
+				echo '</div>';
+
+				// if(current_theme_supports('sp-theme-customiser')) {
+				// 	echo '<b>'.SP()->primitives->admin_text('Use the Customiser option in the Simple:Press Themes menu to customise your colours').'</b>';
+				// }
+    		}
+
+
+			// if(isset($themes[$curTheme['theme']]['ItemId']) && $themes[$curTheme['theme']]['ItemId'] != ''){
+				
+			// 	# any upgrade for this theme using licensing method
+				
+			// 	$sp_theme_name = sanitize_title_with_dashes($curTheme['theme']);
+		
+			// 	$check_for_addon_update = SP()->options->get( 'spl_theme_versioninfo_'.$themes[$curTheme['theme']]['ItemId']);
+				
+			// 	$check_for_addon_update = json_decode($check_for_addon_update);
+		
+			// 	$check_addons_status = SP()->options->get( 'spl_theme_info_'.$themes[$curTheme['theme']]['ItemId']);
+				
+			// 	$check_addons_status = json_decode($check_addons_status);
+                                
+				
+			// 	$update_condition = $check_for_addon_update != '' && isset($check_for_addon_update->new_version) && $check_for_addon_update->new_version != false;
+			// 	$status_condition = $check_addons_status != '' && isset($check_addons_status->license);
+
+			// 	$version_compare = isset($check_for_addon_update->new_version) && (version_compare($check_for_addon_update->new_version, $themes[$curTheme['theme']]['Version'], '>') == 1);
+			
+			// 	if (is_main_site() && $update_condition && $status_condition && $version_compare) {
+					
+			// 		$changelog_link = add_query_arg( array( 'tab' => 'plugin-information', 'plugin' => $sp_theme_name, 'section' => 'changelog', 'TB_iframe' => true, 'width' => 722, 'height' => 949 ), admin_url( 'plugin-install.php' ) );
+                                        
+			// 		echo '<br />';
+					
+			// 		if($check_addons_status->license == 'valid'){
+						
+			// 			echo '<p class="plugin-update-tr"><p class="update-message notice inline notice-warning notice-alt" style="padding: 10px 0px;">';	
+			// 			echo SP()->primitives->admin_text('There is an update for the ').' '.esc_attr($curTheme['theme']).' '.SP()->primitives->admin_text('theme').'.<br />';
+			// 			echo SP()->primitives->admin_text('Version').' '.$check_for_addon_update->new_version.' '.SP()->primitives->admin_text('of the theme is available').'.<br />';
+			// 			echo '<span title="'.SP()->primitives->admin_text('View version full details').'"  class="thickbox open-plugin-details-modal spPluginUpdate" data-width="1000" data-height="0" data-site="'.$ajaxURThem.'" data-label="Simple:Press Plugin Update" data-href="'.esc_url( $changelog_link ).'">'.SP()->primitives->admin_text('View version ').$check_for_addon_update->new_version.SP()->primitives->admin_text(' details').'</span> '.SP()->primitives->admin_text('or').' ';
+			// 			echo '<a href="'.self_admin_url('update-core.php').'" title="'.SP()->primitives->admin_text('update now').'">'.SP()->primitives->admin_text('update now').'</a>.';
+			// 			echo '</p></p>';
+						
+    	// 			} else{
+    					
+			// 			echo '<p class="plugin-update-tr"><p class="update-message notice inline notice-warning notice-alt" style="padding: 10px 0px;">';
+			// 			echo SP()->primitives->admin_text('There is an update for the ').' '.esc_attr($curTheme['theme']).' '.SP()->primitives->admin_text('theme').'.<br />';
+			// 			echo SP()->primitives->admin_text('Version').' '.$check_for_addon_update->new_version.' '.SP()->primitives->admin_text('of the theme is available').'.<br />';
+			// 			echo '<span title="'.SP()->primitives->admin_text('View version full details').'" class="thickbox open-plugin-details-modal spPluginUpdate" data-width="1000" data-height="0" data-site="'.$ajaxURThem.'" data-label="Simple:Press Plugin Update" data-href="'.esc_url( $changelog_link ).'">'.SP()->primitives->admin_text('View version ').$check_for_addon_update->new_version.SP()->primitives->admin_text(' details').'</span>';
+			// 			echo '<br />' . SP()->primitives->admin_text(' Automatic update is unavailable for this theme - most likely because the license key is not present.');
+			// 			echo '</p></p>';
+    	// 			}
+			// 	}
+				
+			// }else{
+				
+			// 	# any upgrade for this theme? in multisite only main site can update
+	    // 		if (is_main_site() && $xml) {
+	    // 			foreach ($xml->themes->theme as $latest) {
+	    // 				if ($themes[$curTheme['theme']]['Name'] == $latest->name) {
+	    // 					if ((version_compare($latest->version, $themes[$curTheme['theme']]['Version'], '>') == 1)) {
+	    // 						echo '<br />';
+	    // 						echo '<p style="padding: 0;">';
+	    // 						echo '<strong>'.SP()->primitives->admin_text('There is an update for the').' '.$themes[$curTheme['theme']]['Name'].' '.SP()->primitives->admin_text('theme').'.</strong> ';
+	    // 						echo SP()->primitives->admin_text('Version').' '.$latest->version.' '.SP()->primitives->admin_text('is available').'. ';
+	    // 						echo SP()->primitives->admin_text('For details and to download please visit').' '.SPPLUGHOME.' '.SP()->primitives->admin_text('or').' '.SP()->primitives->admin_text('go to the').' ';
+	    // 						echo '<a href="'.self_admin_url('update-core.php').'" title="Simple:Press Plugin Update" target="_parent">'.SP()->primitives->admin_text('WordPress updates page').'</a>';
+	    // 						echo '</p>';
+	    // 					}
+	    // 					break;
+	    // 				}
+	    // 			}
+	    // 		}
+			// }
+
+        } else {
+     		echo '<h4>'.SP()->primitives->admin_text('The current theme stylesheet').':<br /><br />'.SPTHEMEBASEDIR.$curTheme['theme'].'/styles/'.$curTheme['style'].'<br /><br />'.SP()->primitives->admin_text('cannot be found. Please correct or select a new theme for proper operation.').'</h4>';
+        }
+?>
+	</div>
 <?php
 	    	foreach ((array) $themes as $theme_file => $theme_data) {
    				# skip cur theme
@@ -229,7 +384,7 @@ function spa_themes_list_form() {
                         }
                     }
 ?>
-					<p class="sf-description" style="padding: 0;">
+					<p class="sf-description" style="">
 						<?php echo $theme_desc; ?>
 					</p>
 					<br />
