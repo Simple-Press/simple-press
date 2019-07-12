f53ec1f0087d0be5ff52dbd6e6d22c32529ae8d4
Sergey Gujvai <serjgujvai@mail.ru>
2019-07-12 17:24:56 +0500

    Integration - Storage Locations (fixed)

--- admin/library/spa-tab-support.php
+++ admin/library/spa-tab-support.php
@@ -101,9 +101,10 @@ function spa_paint_close_panel() {
 	echo '</div>';
 }
 
-function spa_paint_open_fieldset($legend, $displayhelp=false, $helpname='', $displaylegend=true, $subTitle = '') {
-	global $adminhelpfile;
-
+function spa_paint_open_fieldset($legend, $displayhelp = false, $helpname = '', $displaylegend = true, $subTitle = '', $adminhelpfile = false) {
+    if(!$adminhelpfile){
+	    global $adminhelpfile;
+    }
 	echo "<fieldset class='sf-fieldset'>\n";
 	if($displaylegend) {
 		echo "<div class='sf-panel-body-top'><div class='sf-panel-body-top-left'><h4>$legend</h4>";
