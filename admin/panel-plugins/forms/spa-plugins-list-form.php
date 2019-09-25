a8f88ac582afc202b873062af07cd644a51770ea
Ashish Ravi <contactashish13@gmail.com>
2019-09-25 10:50:08 +0530

    dont show "getting started" if help file does not exist

--- admin/panel-plugins/forms/spa-plugins-list-form.php
+++ admin/panel-plugins/forms/spa-plugins-list-form.php
@@ -343,6 +343,11 @@ function spa_plugins_list_form() {
 function sp_paint_plugin_tip($name, $file) {
     # make sure getting started help file name replacement works or bail to keep from returning the main plugin file
 	if ($file == ($xfile = str_replace('-plugin.php', '-help.php', $file))) return '';
+	# if help file does not exist dont show the help option.
+	$path = SP_STORE_DIR.'/'.SP()->plugin->storage['plugins'] . '/' . $xfile;
+	if ( ! realpath( $path ) ) {
+		return '';
+	}
 	$site = wp_nonce_url(SPAJAXURL."plugin-tip&amp;file=$xfile", 'plugin-tip');
 	$atitle = SP()->primitives->admin_text('Getting Started');
 	$htitle = $atitle.' - '.esc_js($name);
