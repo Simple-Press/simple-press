64fc10db0fab9b552769e04e9af0741486ac07b7
Nigel Bahadur <elindydotcom@hotmail.com>
2020-02-18 23:32:00 -0600

    Update build number and version number.

--- sp-control.php
+++ sp-control.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name: 		Simple:Press
  * Plugin URI: 			https://simple-press.com
- * Version: 			6.3.0
+ * Version: 			6.3.1
  * Description: 		The most versatile and feature-rich forums plugin for WordPress
  * Author: 				The Simple:Press Team
  * Author URI: 			https://simple-press.com/about
@@ -340,11 +340,11 @@ if (!class_exists('spcSimplePress')) {
 
 			# version and system control constants
 			define('SPPLUGNAME', 'Simple:Press');
-			define('SPVERSION', '6.3.0');
+			define('SPVERSION', '6.3.1');
                         
             define('SP_SCRIPTS_VERSION', (defined('SP_SCRIPTS_DEBUG') && SP_SCRIPTS_DEBUG) ? SPVERSION . '.' . time() : SPVERSION);
                         
-			define('SPBUILD', 15865);
+			define('SPBUILD', 15866);
 			define('SPSILENT', 15865);
 			define('SPRELEASE', 'Release');
 
