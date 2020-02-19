60f05dc699ee66bf73bcbea17c157e8029e84629
Nigel Bahadur <elindydotcom@hotmail.com>
2020-02-18 23:45:36 -0600

    Make the new version number 6.4 since there is at least one new minor feature.

--- sp-control.php
+++ sp-control.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name: 		Simple:Press
  * Plugin URI: 			https://simple-press.com
- * Version: 			6.3.1
+ * Version: 			6.4.0
  * Description: 		The most versatile and feature-rich forums plugin for WordPress
  * Author: 				The Simple:Press Team
  * Author URI: 			https://simple-press.com/about
@@ -340,7 +340,7 @@ if (!class_exists('spcSimplePress')) {
 
 			# version and system control constants
 			define('SPPLUGNAME', 'Simple:Press');
-			define('SPVERSION', '6.3.1');
+			define('SPVERSION', '6.4.0');
                         
             define('SP_SCRIPTS_VERSION', (defined('SP_SCRIPTS_DEBUG') && SP_SCRIPTS_DEBUG) ? SPVERSION . '.' . time() : SPVERSION);
                         
