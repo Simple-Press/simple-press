fc4bf51d38e7dc19d128631fa905f9edc5090fca
Nigel Bahadur <elindydotcom@hotmail.com>
2021-12-14 19:45:12 -0600

    Forward version number to prepare for next release.

--- sp-control.php
+++ sp-control.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name: 		Simple:Press
  * Plugin URI: 			https://simple-press.com
- * Version: 			6.6.3
+ * Version: 			6.6.4
  * Description: 		The most versatile and feature-rich forums plugin for WordPress
  * Author: 				The Simple:Press Team
  * Author URI: 			https://simple-press.com/about
@@ -340,11 +340,11 @@ if (!class_exists('spcSimplePress')) {
 
 			# version and system control constants
 			define('SPPLUGNAME', 'Simple:Press');
-			define('SPVERSION', '6.6.3');
+			define('SPVERSION', '6.6.4');
                         
             define('SP_SCRIPTS_VERSION', (defined('SP_SCRIPTS_DEBUG') && SP_SCRIPTS_DEBUG) ? SPVERSION . '.' . time() : SPVERSION);
                         
-			define('SPBUILD', 15872);
+			define('SPBUILD', 15873);
 			define('SPSILENT', 15865);
 			define('SPRELEASE', 'Release');
 
