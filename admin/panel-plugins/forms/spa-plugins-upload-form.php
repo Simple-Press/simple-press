3d831a7add2447415e047cbde7744daee0a30297
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-03-13 14:44:36 +0100

    fix: escaped and secured

--- admin/panel-plugins/forms/spa-plugins-upload-form.php
+++ admin/panel-plugins/forms/spa-plugins-upload-form.php
@@ -14,23 +14,23 @@ function spa_plugins_upload_form() {
         spa_paint_open_fieldset(SP()->primitives->admin_text('Upload Plugin'), true, 'upload-plugin');
 ?>
             <div class="sf-alert-block sf-info">
-                <p><?php echo SP()->primitives->admin_text('Upload a Simple:Press plugin in .zip format') ?></p>
-                <p><?php echo SP()->primitives->admin_text('If you have a plugin in a .zip format, you may upload it here') ?></p>
+                <p><?php esc_html_e(SP()->primitives->admin_text('Upload a Simple:Press plugin in .zip format')); ?></p>
+                <p><?php esc_html_e(SP()->primitives->admin_text('If you have a plugin in a .zip format, you may upload it here')); ?></p>
             </div>
-            <form method="post" enctype="multipart/form-data" action="<?php echo self_admin_url('update.php?action=upload-sp-plugin'); ?>" id="sfpluginuploadform" name="sfpluginuploadform">
-                <?php echo sp_create_nonce('forum-plugin_upload'); ?>
+            <form method="post" enctype="multipart/form-data" action="<?php esc_attr_e(self_admin_url('update.php?action=upload-sp-plugin')); ?>" id="sfpluginuploadform" name="sfpluginuploadform">
+                <?php echo '<input type="hidden" name="'.esc_attr().'" value="'.esc_attr(wp_create_nonce()).'" />'; ?>
 				<div class="clear"></div>
 				<div class="sf-upload-file-name"><label class="sp-label"></label></div>
 				<div class="clear"></div>			
                 <label class="sf-button-primary sf-upload-button">
                     <input type="file" id="pluginzip" name="pluginzip" class="sf-hidden-important" />
                     <span class="sf-icon sf-icon-button sf-white sf-upload"></span>
-                    <?php echo SP()->primitives->admin_text('Select file') ?>
+                    <?php echo esc_html(SP()->primitives->admin_text('Select file')); ?>
                 </label>
         	    <input type="button" class="sf-button-primary spPluginUpload" id="saveupload" name="saveupload" value="<?php SP()->primitives->admin_etext('Upload Now'); ?>" onclick="sfloader()" data-target="#saveupload" />
             
                 <div id="sf-loader-gif" style="display: none;">
-                    <img src="<?php echo SPADMINIMAGES . 'sp_WaitBox.gif' ?>" alt="loading" />
+                    <img src="<?php esc_attr_e(SPADMINIMAGES . 'sp_WaitBox.gif'); ?>" alt="loading" />
                 </div>
             </form>
             <script>
