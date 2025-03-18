611f1ffa2f290b2867be7061e509e4715db3764e
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-03-19 00:28:58 +0100

    fix: escape

--- admin/panel-profiles/forms/spa-profiles-tabs-menus-form.php
+++ admin/panel-profiles/forms/spa-profiles-tabs-menus-form.php
@@ -41,7 +41,7 @@ function spa_profiles_tabs_menus_form() {
 			<?php if ($tabs) { ?>
 				/* now run sortable on tabs and menus so they're guaranteed to be populate */
 				$("input#spTabsOrder").val($("#tabsList").sortable('serialize'));
-				num = <?php echo count($tabs); ?>;
+				num = <?php echo esc_js(count($tabs)); ?>;
 				for (i=0; i<=num; i++) {
 					$("input#spMenusOrder"+i).val($("#sf-list-sortable"+i).sortable('serialize'));
 				}
@@ -63,51 +63,51 @@ function spa_profiles_tabs_menus_form() {
 	spa_paint_open_tab(/*SP()->primitives->admin_text('Profiles').' - '.*/SP()->primitives->admin_text('Profile Tabs & Menus'), true);
 		spa_paint_open_panel();
 			spa_paint_open_fieldset(SP()->primitives->admin_text('Profile Menu Order'), true, 'profile-menus');
-				echo '<div class="sf-alert-block sf-info">'.SP()->primitives->admin_text('Here you can set the order of Profile Tabs and Menus by dragging and dropping below.  Additionally, you can edit any of the Tabs or Menus.').'</div>';
+				echo '<div class="sf-alert-block sf-info">'.esc_html(SP()->primitives->admin_text('Here you can set the order of Profile Tabs and Menus by dragging and dropping below.  Additionally, you can edit any of the Tabs or Menus.')).'</div>';
 
 				if (!empty($tabs)) {
 					echo '<ul id="tabsList" class="tabsList sf-list">';
 					foreach ($tabs as $tindex => $tab) {
 						echo '<li id="tab-'.$tindex.'" class="sf-list-item-depth-0">';
                         $class = ($tab['display']) ? '' : ' sf-list-item-disabled';
-						echo "<div class='sf-list-item$class'>";
-						echo '<span class="sf-item-name">'.$tab['name'].'</span>';
+						echo "<div class='sf-list-item".esc_attr($class)."'>";
+						echo '<span class="sf-item-name">'.esc_html($tab['name']).'</span>';
 						echo '<span class="sf-item-controls">';
-						echo '<span class="sf-item-type">'.SP()->primitives->admin_text('Tab').'</span>';
-						echo '<a class="sf-item-edit spLayerToggle" data-target="item-edit-'.$tindex.'">Edit Menu</a>';
-						echo '<input type="hidden" size="70" id="spMenusOrder'.$tindex.'" name="spMenusOrder'.$tindex.'" />';
+						echo '<span class="sf-item-type">'.esc_html(SP()->primitives->admin_text('Tab')).'</span>';
+						echo '<a class="sf-item-edit spLayerToggle" data-target="item-edit-'.esc_attr($tindex).'">'.esc_html(SP()->primitives->admin_text('Edit Menu')).'</a>';
+						echo '<input type="hidden" size="70" id="spMenusOrder'.esc_attr($tindex).'" name="spMenusOrder'.esc_attr($tindex).'" />';
 						echo '</span>';
 						echo '</div>';
-						echo '<div id="item-edit-'.$tindex.'" class="sf-list-item-settings sf-inline-edit">';
-						echo '<p class="sf-description">'.SP()->primitives->admin_text('Tab Name').'<br /><input type="text" class="sfpostcontrol" id="tab-name-'.$tindex.'" name="tab-name-'.$tindex.'" value="'.SP()->displayFilters->title($tab['name']).'" /></p>';
-                        echo '<input type="hidden" id="tab-slug-'.$tindex.'" name="tab-slug-'.$tindex.'" value="'.esc_attr($tab['slug']).'" />';
-						echo '<p class="sf-description">'.SP()->primitives->admin_text('Tab Auth').'<br /><input type="text" class="sfpostcontrol" id="tab-auth-'.$tindex.'" name="tab-auth-'.$tindex.'" value="'.SP()->displayFilters->title($tab['auth']).'" /></p>';
+						echo '<div id="item-edit-'.esc_attr($tindex).'" class="sf-list-item-settings sf-inline-edit">';
+						echo '<p class="sf-description">'.esc_html(SP()->primitives->admin_text('Tab Name')).'<br /><input type="text" class="sfpostcontrol" id="tab-name-'.esc_attr($tindex).'" name="tab-name-'.esc_attr($tindex).'" value="'.esc_attr(SP()->displayFilters->title($tab['name'])).'" /></p>';
+                        echo '<input type="hidden" id="tab-slug-'.esc_attr($tindex).'" name="tab-slug-'.esc_attr($tindex).'" value="'.esc_attr($tab['slug']).'" />';
+						echo '<p class="sf-description">'.esc_html(SP()->primitives->admin_text('Tab Auth')).'<br /><input type="text" class="sfpostcontrol" id="tab-auth-'.esc_attr($tindex).'" name="tab-auth-'.esc_attr($tindex).'" value="'.esc_attr(SP()->displayFilters->title($tab['auth'])).'" /></p>';
 						$checked = ($tab['display']) ? $checked = 'checked="checked" ' : '';
-						echo '<p class="sf-description"><input type="checkbox" '.$checked.'name="tab-display-'.$tindex.'" id="sf-tab-display-'.$tindex.'" /><label for="sf-tab-display-'.$tindex.'">'.SP()->primitives->admin_text('Display Tab').'</label></p>';
-						echo '<p><a class="spLayerToggle" data-target="item-edit-'.$tindex.'" >'.SP()->primitives->admin_text('Close').'</a></p>';
+						echo '<p class="sf-description"><input type="checkbox" '.esc_attr($checked).'name="tab-display-'.esc_attr($tindex).'" id="sf-tab-display-'.esc_attr($tindex).'" /><label for="sf-tab-display-'.esc_attr($tindex).'">'.esc_html(SP()->primitives->admin_text('Display Tab')).'</label></p>';
+						echo '<p><a class="spLayerToggle" data-target="item-edit-'.esc_attr($tindex).'" >'.esc_html(SP()->primitives->admin_text('Close')).'</a></p>';
 						echo '</div>';
 
 						# now output any menus on the tab
-						echo '<ul id="sf-list-sortable'.$tindex.'" class="sf-list-sortable sf-list">';
+						echo '<ul id="sf-list-sortable'.esc_attr($tindex).'" class="sf-list-sortable sf-list">';
 						if (!empty($tab['menus'])) {
 							foreach ($tab['menus'] as $mindex => $menu) {
-								echo '<li id="tab'.$tindex.'-'.$mindex.'" class="sf-list-item-depth-1">';
+								echo '<li id="tab'.esc_attr($tindex).'-'.esc_attr($mindex).'" class="sf-list-item-depth-1">';
                                 $class = ($menu['display']) ? '' : ' sf-list-item-disabled';
-								echo "<div class='sf-list-item$class'>";
-								echo '<span class="sf-item-name">'.$menu['name'].'</span>';
+								echo "<div class='sf-list-item".esc_attr($class)."'>";
+								echo '<span class="sf-item-name">'.esc_html($menu['name']).'</span>';
 								echo '<span class="sf-item-controls sf-mr-5">';
-								echo '<span class="sf-item-type">'.SP()->primitives->admin_text('Menu').'</span>';
-								echo '<a class="sf-item-edit spLayerToggle" data-target="item-edit-'.$tindex.'-'.$mindex.'" >Edit Menu</a>';
+								echo '<span class="sf-item-type">'.esc_html(SP()->primitives->admin_text('Menu')).'</span>';
+								echo '<a class="sf-item-edit spLayerToggle" data-target="item-edit-'.esc_attr($tindex).'-'.esc_attr($mindex).'" >'.esc_html(SP()->primitives->admin_text('Edit Menu')).'</a>';
 								echo '</span>';
 								echo '</div>';
-								echo '<div id="item-edit-'.$tindex.'-'.$mindex.'" class="sf-list-item-settings sf-inline-edit">';
-								echo '<p class="sf-description">'.SP()->primitives->admin_text('Menu Name').'<br /><input type="text" class="sfpostcontrol" id="menu-name-'.$tindex.'-'.$mindex.'" name="menu-name-'.$tindex.'-'.$mindex.'" value="'.SP()->displayFilters->title($menu['name']).'" /></p>';
-                                echo '<input type="hidden" id="menu-slug-'.$tindex.'-'.$mindex.'" name="menu-slug-'.$tindex.'-'.$mindex.'" value="'.esc_attr($menu['slug']).'" />';
-								echo '<p class="sf-description">'.SP()->primitives->admin_text('Menu Auth').'<br /><input type="text" class="sfpostcontrol" id="menu-auth-'.$tindex.'-'.$mindex.'" name="menu-auth-'.$tindex.'-'.$mindex.'" value="'.SP()->displayFilters->title($menu['auth']).'" /></p>';
-								echo '<p class="sf-description">'.SP()->primitives->admin_text('Menu Form').'<br /><input type="text" class="sfpostcontrol" id="menu-form-'.$tindex.'-'.$mindex.'" name="menu-form-'.$tindex.'-'.$mindex.'" value="'.esc_attr($menu['form']).'" /></p>';
+								echo '<div id="item-edit-'.esc_attr($tindex).'-'.esc_attr($mindex).'" class="sf-list-item-settings sf-inline-edit">';
+								echo '<p class="sf-description">'.esc_html(SP()->primitives->admin_text('Menu Name')).'<br /><input type="text" class="sfpostcontrol" id="menu-name-'.esc_attr($tindex).'-'.esc_attr($mindex).'" name="menu-name-'.esc_attr($tindex).'-'.esc_attr($mindex).'" value="'.esc_attr(SP()->displayFilters->title($menu['name'])).'" /></p>';
+                                echo '<input type="hidden" id="menu-slug-'.esc_attr($tindex).'-'.esc_attr($mindex).'" name="menu-slug-'.esc_attr($tindex).'-'.esc_attr($mindex).'" value="'.esc_attr($menu['slug']).'" />';
+								echo '<p class="sf-description">'.esc_html(SP()->primitives->admin_text('Menu Auth')).'<br /><input type="text" class="sfpostcontrol" id="menu-auth-'.esc_attr($tindex).'-'.esc_attr($mindex).'" name="menu-auth-'.esc_attr($tindex).'-'.esc_attr($mindex).'" value="'.esc_attr(SP()->displayFilters->title($menu['auth'])).'" /></p>';
+								echo '<p class="sf-description">'.esc_html(SP()->primitives->admin_text('Menu Form')).'<br /><input type="text" class="sfpostcontrol" id="menu-form-'.esc_attr($tindex).'-'.esc_attr($mindex).'" name="menu-form-'.esc_attr($tindex).'-'.esc_attr($mindex).'" value="'.esc_attr($menu['form']).'" /></p>';
 								$checked = ($menu['display']) ? $checked = 'checked="checked" ' : '';
-								echo '<p class="sf-description"><input type="checkbox" '.$checked.'name="menu-display-'.$tindex.'-'.$mindex.'" id="sf-list-display-'.$tindex.'-'.$mindex.'" /><label for="sf-list-display-'.$tindex.'-'.$mindex.'">'.SP()->primitives->admin_text('Display Menu').'</label></p>';
-								echo '<p><a class="spLayerToggle" data-target="item-edit-'.$tindex.'-'.$mindex.'" >'.SP()->primitives->admin_text('Close').'</a></p>';
+								echo '<p class="sf-description"><input type="checkbox" '.esc_attr($checked).'name="menu-display-'.esc_attr($tindex).'-'.esc_attr($mindex).'" id="sf-list-display-'.esc_attr($tindex).'-'.esc_attr($mindex).'" /><label for="sf-list-display-'.esc_attr($tindex).'-'.esc_attr($mindex).'">'.esc_html(SP()->primitives->admin_text('Display Menu')).'</label></p>';
+								echo '<p><a class="spLayerToggle" data-target="item-edit-'.esc_attr($tindex).'-'.esc_attr($mindex).'" >'.esc_html(SP()->primitives->admin_text('Close')).'</a></p>';
 								echo '</div>';
 								echo '</li>';
 							}
