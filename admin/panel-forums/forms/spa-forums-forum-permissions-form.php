90af426759178f4a2e8b4d1eaee81ac7351a13d1
Peder Sjögren <peder.sjogren@causalityagency.com>
2025-03-18 16:46:22 +0100

    Escape spa-forums-forum-permissions-form.php

--- admin/panel-forums/forms/spa-forums-forum-permissions-form.php
+++ admin/panel-forums/forms/spa-forums-forum-permissions-form.php
@@ -2,8 +2,6 @@
 /*
 Simple:Press
 Admin Forums Forum Permission Form
-$LastChangedDate: 2017-08-05 17:36:04 -0500 (Sat, 05 Aug 2017) $
-$Rev: 15488 $
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -11,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 # function to display the current forum permission set.  It is hidden until the permission set link is clicked.
-# additional forms to add, edit or delete these permission set are further hidden belwo the permission set information
+# additional forms to add, edit or delete these permission set are further hidden below the permission set information
 function spa_forums_view_forums_permission_form($forum_id)
 {
 	$forum = SP()->DB->table(SPFORUMS, "forum_id=$forum_id", 'row');
@@ -26,7 +24,11 @@ function spa_forums_view_forums_permission_form($forum_id)
 ?>
 					<table class="sfmaintable sf-forum-perms-table">
 						<tr>
-							<td class="sf-text-al-center" colspan="3"><strong><?php SP()->primitives->admin_etext('Current permission set for forum').' '.SP()->displayFilters->title($forum->forum_name); ?></strong></td>
+							<td class="sf-text-al-center" colspan="3">
+								<strong><?php SP()->primitives->admin_etext('Current permission set for forum'); 
+									echo ' '.esc_html(SP()->displayFilters->title($forum->forum_name)); ?>
+								</strong>
+							</td>
 						</tr>
 <?php
 					foreach ($perms as $perm) {
@@ -34,20 +36,20 @@ function spa_forums_view_forums_permission_form($forum_id)
 						$role = spa_get_role_row($perm->permission_role);
 ?>
 						<tr>
-							<td class="sflabel"><?php echo SP()->displayFilters->title($usergroup->usergroup_name); ?> => <?php echo SP()->displayFilters->title($role->role_name); ?></td>
+							<td class="sflabel"><?php echo esc_html(SP()->displayFilters->title($usergroup->usergroup_name)); ?> => <?php echo esc_html(SP()->displayFilters->title($role->role_name)); ?></td>
 							<td class="sf-text-al-center" >
 <?php
                                 $base = wp_nonce_url(SPAJAXURL.'forums-loader', 'forums-loader');
 								$target = "curperm-$perm->permission_id";
 								$image = SPADMINIMAGES;
 ?>
-								<input type="button" class="sf-button-secondary spStackBtnLong spLoadForm" value="<?php SP()->primitives->admin_etext('Edit Permission Set'); ?>" data-form="editperm" data-url="<?php echo $base; ?>" data-target="<?php echo $target; ?>" data-img="<?php echo $image; ?>" data-id="<?php echo $perm->permission_id; ?>" data-open="" />
-								<input type="button" class="sf-button-secondary spStackBtnLong spLoadForm" value="<?php SP()->primitives->admin_etext('Delete Permission Set'); ?>" data-form="delperm" data-url="<?php echo $base; ?>" data-target="<?php echo $target; ?>" data-img="<?php echo $image; ?>" data-id="<?php echo $perm->permission_id; ?>" data-open="" />
+								<input type="button" class="sf-button-secondary spStackBtnLong spLoadForm" value="<?php SP()->primitives->admin_etext('Edit Permission Set'); ?>" data-form="editperm" data-url="<?php echo esc_attr($base); ?>" data-target="<?php echo esc_attr($target); ?>" data-img="<?php echo esc_attr($image); ?>" data-id="<?php echo esc_attr($perm->permission_id); ?>" data-open="" />
+								<input type="button" class="sf-button-secondary spStackBtnLong spLoadForm" value="<?php SP()->primitives->admin_etext('Delete Permission Set'); ?>" data-form="delperm" data-url="<?php echo esc_attr($base); ?>" data-target="<?php echo esc_attr($target); ?>" data-img="<?php echo esc_attr($image); ?>" data-id="<?php echo esc_attr($perm->permission_id); ?>" data-open="" />
 							</td>
 			   			</tr>
 						<tr class="sfinline-form"> <!-- This row will hold hidden forms for the current forum permission set -->
 						  	<td colspan="3">
-								<div id="curperm-<?php echo $perm->permission_id; ?>">
+								<div id="curperm-<?php echo esc_attr($perm->permission_id); ?>">
 							</td>
 						</tr>
 					<?php } ?>
@@ -66,12 +68,12 @@ function spa_forums_view_forums_permission_form($forum_id)
 								$target = "newperm-$forum->forum_id";
 								$image = SPADMINIMAGES;
 ?>
-								<input type="button" class="sf-button-secondary spStackBtn spLoadForm" value="<?php SP()->primitives->admin_etext('Add Permission'); ?>" data-form="addperm" data-url="<?php echo $base; ?>" data-target="<?php echo $target; ?>" data-img="<?php echo $image; ?>" data-id="<?php echo $forum->forum_id; ?>" data-open="open" />
+								<input type="button" class="sf-button-secondary spStackBtn spLoadForm" value="<?php SP()->primitives->admin_etext('Add Permission'); ?>" data-form="addperm" data-url="<?php echo esc_attr($base); ?>" data-target="<?php echo esc_attr($target); ?>" data-img="<?php echo esc_attr($image); ?>" data-id="<?php echo esc_attr($forum->forum_id); ?>" data-open="open" />
 			   				</td>
 						</tr>
 						<tr class="sfinline-form"> <!-- This row will hold ajax forms for adding a new forum permission set -->
 						  	<td colspan="3">
-								<div id="newperm-<?php echo $forum->forum_id; ?>">
+								<div id="newperm-<?php echo esc_attr($forum->forum_id); ?>">
 								</div>
 							</td>
 						</tr>
@@ -83,7 +85,7 @@ function spa_forums_view_forums_permission_form($forum_id)
 ?>
 	<form>
 		<div class="sf-form-submit-bar">
-            <input type="button" class="sf-button-primary spCancelForm" data-target="#forum-<?php echo $forum->forum_id; ?>" name="forumcancel<?php echo $forum->forum_id; ?>" value="<?php SP()->primitives->admin_etext('Cancel'); ?>" />
+            <input type="button" class="sf-button-primary spCancelForm" data-target="#forum-<?php echo esc_attr($forum->forum_id); ?>" name="forumcancel<?php echo esc_attr($forum->forum_id); ?>" value="<?php SP()->primitives->admin_etext('Cancel'); ?>" />
 		</div>
 	</form>
 	<?php spa_paint_close_tab(); ?>
