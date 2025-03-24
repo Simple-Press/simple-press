4059cf24cba4aa253783b380c0aa27f0dfc4eb04
Peder Sjögren <peder.sjogren@causalityagency.com>
2025-03-24 23:06:43 +0100

    esc spa-components-special-ranks-del-form.php

--- admin/panel-components/forms/spa-components-special-ranks-del-form.php
+++ admin/panel-components/forms/spa-components-special-ranks-del-form.php
@@ -2,8 +2,6 @@
 /*
 Simple:Press
 Admin Components Special Rank Delete Member Form
-$LastChangedDate: 2017-12-28 11:37:41 -0600 (Thu, 28 Dec 2017) $
-$Rev: 15601 $
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,14 +11,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 function spa_components_sr_del_members_form($rank_id) {
 ?>
 <script>
-   	spj.loadAjaxForm('sfmemberdel<?php echo $rank_id; ?>', 'sfreloadfr');
+   	spj.loadAjaxForm('sfmemberdel<?php echo esc_js($rank_id); ?>', 'sfreloadfr');
 </script>
 <?php
 	spa_paint_options_init();
 
-    $ajaxURL = wp_nonce_url(SPAJAXURL."components-loader&amp;saveform=specialranks&amp;targetaction=delmember&amp;id=$rank_id", 'components-loader');
+    $ajaxURL = wp_nonce_url(SPAJAXURL."components-loader&amp;saveform=specialranks&amp;targetaction=delmember&amp;id=" . esc_attr($rank_id), 'components-loader');
 ?>
-	<form action="<?php echo esc_url($ajaxURL); ?>" method="post" id="sfmemberdel<?php echo $rank_id; ?>" name="sfmemberdel<?php echo $rank_id; ?>">
+	<form action="<?php echo esc_url($ajaxURL); ?>" method="post" id="sfmemberdel<?php echo esc_attr($rank_id); ?>" name="sfmemberdel<?php echo esc_attr($rank_id); ?>">
 <?php
                 spa_paint_open_nohead_tab(true, '');
 		sp_echo_create_nonce('special-rank-del');
@@ -34,11 +32,11 @@ function spa_components_sr_del_members_form($rank_id) {
 ?>
 					<div class="clearboth"></div>
 <?php
-        $loc = 'sfrankshow-'.$rank_id;
+        $loc = 'sfrankshow-' . $rank_id;
 ?>
             <div class="sf-controls">
-		<input type="submit" class="sf-button-primary spSpecialRankDel" id="sfmemberdel<?php echo $rank_id; ?>" name="sfmemberdel<?php echo $rank_id; ?>" data-target="#dmember_id<?php echo $rank_id; ?>" value="<?php SP()->primitives->admin_etext('Remove Members'); ?>" />
-		<input type="button" class="sf-button-primary spCancelForm" data-target="#members-<?php echo $rank_id; ?>" data-loc="<?php echo $loc; ?>" id="sfmemberdel<?php echo $rank_id; ?>" name="addmemberscancel<?php echo $rank_id; ?>" value="<?php SP()->primitives->admin_etext('Cancel'); ?>" />
+		<input type="submit" class="sf-button-primary spSpecialRankDel" id="sfmemberdel<?php echo esc_attr($rank_id); ?>" name="sfmemberdel<?php echo esc_attr($rank_id); ?>" data-target="#dmember_id<?php echo esc_attr($rank_id); ?>" value="<?php SP()->primitives->admin_etext('Remove Members'); ?>" />
+		<input type="button" class="sf-button-primary spCancelForm" data-target="#members-<?php echo esc_attr($rank_id); ?>" data-loc="<?php echo esc_attr($loc); ?>" id="sfmemberdel<?php echo esc_attr($rank_id); ?>" name="addmemberscancel<?php echo esc_attr($rank_id); ?>" value="<?php SP()->primitives->admin_etext('Cancel'); ?>" />
             </div>
 	 <?php
         spa_paint_close_container();
