431c284155d25488a27eb5cf0aa7ec8d5a6a4179
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-05-26 15:44:08 +0200

    fix: escape

--- admin/panel-profiles/support/spa-profiles-prepare.php
+++ admin/panel-profiles/support/spa-profiles-prepare.php
@@ -26,6 +26,7 @@ function spa_get_tabsmenus_data() {
 	return $tabs;
 }
 
+
 function spa_get_avatars_data() {
 	$sfavatars = SP()->options->get('sfavatars');
 	if(empty($sfavatars['sfavatarpriority'])) {
@@ -46,96 +47,89 @@ function spa_paint_avatar_pool() {
 	}
 
 	# start the table display
-	$out.= '<table class="widefat sf-table-small sf-table-mobile"><thead><tr>';
-	$out.= '<th>'.SP()->primitives->admin_text('Avatar').'</th>';
-	$out.= '<th>'.SP()->primitives->admin_text('Filename').'</th>';
-	$out.= '<th>'.SP()->primitives->admin_text('Remove').'</th>';
-	$out.= '</tr></thead>';
-	$out.= '<tbody>';
+	echo '<table class="widefat sf-table-small sf-table-mobile"><thead><tr>';
+	echo '<th>'.esc_html(SP()->primitives->admin_text('Avatar')).'</th>';
+	echo '<th>'.esc_html(SP()->primitives->admin_text('Filename')).'</th>';
+	echo '<th>'.esc_html(SP()->primitives->admin_text('Remove')).'</th>';
+	echo '</tr></thead>';
+	echo '<tbody>';
 	while (false !== ($file = readdir($dlist))) {
 		if ($file != "." && $file != "..") {
-			$out.= '<tr>';
-//			ashow(SPAVATARPOOLURL);
-//			ashow($file);
-			$out.= '<td class="spWFBorder"><div class="sf-avatar"><img class="sfavatarpool" src="'.esc_url(SPAVATARPOOLURL.$file).'" alt="" /></div></td>';
-			$out.= '<td class="spWFBorder sflabel">';
-			$out.= $file;
-			$out.= '</td>';
-			$out.= '<td class="spWFBorder"><div class="sf-item-controls">';
-			$site = esc_url(wp_nonce_url(SPAJAXURL."profiles&amp;targetaction=delavatar&amp;file=$file", 'profiles'));
-			$out.= '<span title="'.SP()->primitives->admin_text('Delete Avatar').'" class="sf-icon sf-delete spDeleteRowReload" data-url="'.$site.'" data-reload="sfreloadpool"></span>';
-			$out.= '</div></td>';
-			$out.= '</tr>';
+			echo '<tr>';
+			echo '<td class="spWFBorder"><div class="sf-avatar"><img class="sfavatarpool" src="'.esc_url(SPAVATARPOOLURL.$file).'" alt="" /></div></td>';
+			echo '<td class="spWFBorder sflabel">';
+			echo esc_html($file);
+			echo '</td>';
+			echo '<td class="spWFBorder"><div class="sf-item-controls">';
+			$site = wp_nonce_url(SPAJAXURL."profiles&amp;targetaction=delavatar&amp;file=$file", 'profiles');
+			echo '<span title="'.esc_html(SP()->primitives->admin_text('Delete Avatar')).'" class="sf-icon sf-delete spDeleteRowReload" data-url="'.esc_url($site).'" data-reload="sfreloadpool"></span>';
+			echo '</div></td>';
+			echo '</tr>';
 		}
 	}
-	$out.= '</tbody></table>';
+	echo '</tbody></table>';
 	closedir($dlist);
-
-	echo $out;
 }
 
 function spa_paint_avatar_defaults() {
-	$out = '';
-
 	# Open avatar defaults folder and get cntents for matching
 	$path = SP_STORE_DIR.'/'.SP()->plugin->storage['avatars'].'/defaults/';
 	$dlist = @opendir($path);
 	if (!$dlist) {
-		echo '<table><tr><td class="sflabel"><strong>'.SP()->primitives->admin_text('The avatar defaults folder does not exist').'</strong></td></tr></table>';
+		echo '<table><tr><td class="sflabel"><strong>'.esc_html(SP()->primitives->admin_text('The avatar defaults folder does not exist')).'</strong></td></tr></table>';
 		return;
 	}
 
 	$def = SP()->options->get('spDefAvatars');
 
 	# start the table display
-	$out.= '<div id="av-browser">';
+	echo '<div id="av-browser">';
 
 	while (false !== ($file = readdir($dlist))) {
 		if ($file != "." && $file != "..") {
 			$found = false;
 			$border = (in_array($file, $def) ? '2px solid red' : '2px solid lightgray');
 
-			$out.= '<div class="av-file" style="text-align:left;border:'.$border.';margin:5px;padding:6px;float:left;">';
-				$out.= '<div class="sf-form-row">';
-				$out.= '<div class="sf-avatar"><img src="'.esc_url(SPAVATARURL.'defaults/'.$file).'" alt="" /></div>';
-				$site = esc_url(wp_nonce_url(SPAJAXURL."profiles&amp;targetaction=deldefault&amp;file=$file", 'profiles'));
-				$out.= '<span class="sf-icon sf-delete spDeleteRowReload sf-pull-right" data-url="'.$site.'" data-reload="sfreloadav"></span>';
-				$out.= '</div>';
+			echo '<div class="av-file" style="text-align:left;border:'.esc_html($border).';margin:5px;padding:6px;float:left;">';
+				echo '<div class="sf-form-row">';
+				echo '<div class="sf-avatar"><img src="'.esc_url(SPAVATARURL.'defaults/'.$file).'" alt="" /></div>';
+				$site = wp_nonce_url(SPAJAXURL."profiles&amp;targetaction=deldefault&amp;file=$file", 'profiles');
+				echo '<span class="sf-icon sf-delete spDeleteRowReload sf-pull-right" data-url="'.esc_url($site).'" data-reload="sfreloadav"></span>';
+				echo '</div>';
 
 				$fileid = str_replace('.', 'z1z2z3', $file);
 				$checked = ($def['admin']==$file) ? ' checked="checked" ' : '';
 				if ($def['admin']==$file) $found=true;
-				$out.= '<input type="radio" value="admin" class="spCheckAvatarDefaults" id="adm-'.$fileid.'" name="'.$fileid.'"'.$checked.'>';
-				$out.= '<label for="adm-'.$fileid.'">'.SP()->primitives->admin_text('Admin').'</label><br>';
+
+				echo '<input type="radio" value="admin" class="spCheckAvatarDefaults" id="adm-'.esc_html($fileid).'" name="'.esc_html($fileid).'"'.esc_html($checked).'>';
+				echo '<label for="adm-'.esc_html($fileid).'">'.esc_html(SP()->primitives->admin_text('Admin')).'</label><br>';
 
 				$checked = ($def['mod']==$file) ? ' checked="checked" ' : '';
 				if ($def['mod']==$file) $found=true;
-				$out.= '<input type="radio" value="mod" class="spCheckAvatarDefaults" id="mod-'.$fileid.'" name="'.$fileid.'"'.$checked.'>';
-				$out.= '<label for="mod-'.$fileid.'">'.SP()->primitives->admin_text('Moderator').'</label><br>';
+				echo '<input type="radio" value="mod" class="spCheckAvatarDefaults" id="mod-'.esc_html($fileid).'" name="'.esc_html($fileid).'"'.esc_html($checked).'>';
+				echo '<label for="mod-'.esc_html($fileid).'">'.esc_html(SP()->primitives->admin_text('Moderator')).'</label><br>';
 
 				$checked = ($def['member']==$file) ? ' checked="checked" ' : '';
 				if ($def['member']==$file) $found=true;
-				$out.= '<input type="radio" value="member" class="spCheckAvatarDefaults" id="mem-'.$fileid.'" name="'.$fileid.'"'.$checked.'>';
-				$out.= '<label for="mem-'.$fileid.'">'.SP()->primitives->admin_text('Member').'</label><br>';
+				echo '<input type="radio" value="member" class="spCheckAvatarDefaults" id="mem-'.esc_html($fileid).'" name="'.esc_html($fileid).'"'.esc_html($checked).'>';
+				echo '<label for="mem-'.esc_html($fileid).'">'.esc_html(SP()->primitives->admin_text('Member')).'</label><br>';
 
 				$checked = ($def['guest']==$file) ? ' checked="checked" ' : '';
 				if ($def['guest']==$file) $found=true;
-				$out.= '<input type="radio" value="guest" class="spCheckAvatarDefaults" id="gue-'.$fileid.'" name="'.$fileid.'"'.$checked.'>';
-				$out.= '<label for="gue-'.$fileid.'">'.SP()->primitives->admin_text('Guest').'</label><br>';
+				echo '<input type="radio" value="guest" class="spCheckAvatarDefaults" id="gue-'.esc_html($fileid).'" name="'.esc_html($fileid).'"'.esc_html($checked).'>';
+				echo '<label for="gue-'.esc_html($fileid).'">'.esc_html(SP()->primitives->admin_text('Guest')).'</label><br>';
 
 				$checked = (!$found) ? ' checked="checked" ' : '';
-				$out.= '<input type="radio" value="none" class="spCheckAvatarDefaults" id="non-'.$fileid.'" name="'.$fileid.'"'.$checked.'>';
-				$out.= '<label for="non-'.$fileid.'">'.SP()->primitives->admin_text('None').'</label>';
+				echo '<input type="radio" value="none" class="spCheckAvatarDefaults" id="non-'.esc_html($fileid).'" name="'.esc_html($fileid).'"'.esc_html($checked).'>';
+				echo '<label for="non-'.esc_html($fileid).'">'.esc_html(SP()->primitives->admin_text('None')).'</label>';
 
-				$out.= '<div class="clearboth"></div>';
+				echo '<div class="clearboth"></div>';
 
-			$out.= '</div>';
+			echo '</div>';
 		}
 	}
 
-	$out.= '<div class="clearboth"></div>';
-	$out.= '</div>';
+	echo '<div class="clearboth"></div>';
+	echo '</div>';
 	closedir($dlist);
-
-	echo $out;
 }
