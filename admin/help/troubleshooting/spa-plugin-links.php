703e31a81e83eca0b9bdd70562c29667666dc384
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-06-23 12:23:52 +0200

    fix: spa-plugins-links.php

--- admin/help/troubleshooting/spa-plugin-links.php
+++ admin/help/troubleshooting/spa-plugin-links.php
@@ -1,10 +1,10 @@
 <div class="codex">
 
-	<img class="spLeft" src="<?php echo SPCOMMONIMAGES; ?>plugin.png" alt="" title="" />
+	<img class="spLeft" src="<?php echo esc_html(SPCOMMONIMAGES); ?>plugin.png" alt="" title="" />
 	<div class="codex-head">Simple:Press Plugins</div>
 	<div class="clearboth"></div>
 
-	<p>Please visit our <a href="<?php echo(SPHOMESITE); ?>/store/add-ons/">Plugin Library</a></p>
+	<p>Please visit our <a href="<?php echo esc_html(SPHOMESITE); ?>/store/add-ons/">Plugin Library</a></p>
 	<p>With nearly 70 plugins available there is something every forum administrator needs in our plugin library.
 	From Subscriptions to Post Editors, Private Messaging to Polls, File and Image Uploading to enhanced Search and more!</p>
 
