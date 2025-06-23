95a8547326391c9cd7137116f549cf8e171d4de7
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-06-23 16:26:21 +0200

    fix: spa-forums-create-forum-form.php

--- admin/panel-forums/forms/spa-forums-create-forum-form.php
+++ admin/panel-forums/forms/spa-forums-create-forum-form.php
@@ -63,7 +63,7 @@ function spa_forums_create_forum_form() {
 					echo "<div class='sf-form-row'>\n";
 					echo "<label>".esc_html(SP()->primitives->admin_text('Select group new forum will belong to'))."</label>\n";
 					echo '<select class="spForumSetSequence" tabindex="'.esc_attr($tab).'" name="group_id">';
-					echo spa_create_group_select(0, 1);
+                    spa_create_group_select(0, 1);
 					echo "</select>\n";
 					echo '<div class="clearboth"></div>';
 					echo '</div>';
@@ -74,7 +74,24 @@ function spa_forums_create_forum_form() {
 					echo "<div class='sf-form-row'>\n";
 					echo "<label>".esc_html(SP()->primitives->admin_text('Select forum new subforum will belong to')).":</label>\n";
 					echo '<select class="spForumSetSequence" tabindex="'.esc_attr($tab).'" name="forum_id">';
-					echo wp_kses_post( sp_render_group_forum_select(false, false, false, true) );
+                        echo wp_kses(
+                            sp_render_group_forum_select(false, false, false, true),
+                            array(
+                                'select' => array(
+                                    'id' => array(),
+                                    'class' => array(),
+                                    'name' => array(),
+                                    'onchange' => array(),
+                                ),
+                                'option' => array(
+                                    'value' => array(),
+                                ),
+                                'optgroup' => array(
+                                    'class' => array(),
+                                    'label' => array(),
+                                ),
+                            )
+                        ) ;
 					echo "</select>\n";
 					echo '<div class="clearboth"></div>';
 					echo '</div>';
