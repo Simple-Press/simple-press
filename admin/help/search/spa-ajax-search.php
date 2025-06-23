7a5190e1491445a0d5417d02c9eba75ec0028c2d
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-06-23 09:36:33 +0200

    fix: spa-ajax-search.php

--- admin/help/search/spa-ajax-search.php
+++ admin/help/search/spa-ajax-search.php
@@ -16,7 +16,7 @@ spa_paint_open_tab(SP()->primitives->admin_text('Forum Admin Task Glossary'));
 	spa_paint_open_panel();
 		echo '<div class="helpAndFAQ">';
 			spa_paint_open_fieldset(SP()->primitives->admin_text('Task Glossary'), false);
-				echo '<p><b>'.SP()->primitives->front_text("Select the item to display tasks for from the list below").'</b></p>';
+				echo '<p><b>'.esc_html(SP()->primitives->front_text("Select the item to display tasks for from the list below")).'</b></p>';
 
 				$sql = "SELECT * FROM ".SPADMINKEYWORDS." ORDER BY keyword";
 				$keywords = SP()->DB->select($sql);
@@ -24,8 +24,8 @@ spa_paint_open_tab(SP()->primitives->admin_text('Forum Admin Task Glossary'));
 					echo '<ul class="key-word-list">';
 					foreach ($keywords as $keyword) {
 						$ajaxPostURL = wp_nonce_url(SPAJAXURL.'adminkeywords&targetaction=gettasks&keyword='.urlencode($keyword->keyword).'&id='.$keyword->id, 'adminkeywords');
-						echo "<li class='key-word' data-url='$ajaxPostURL'>";
-						echo $keyword->keyword.'</li>';
+						echo "<li class='key-word' data-url='".esc_url($ajaxPostURL)."'>";
+						echo esc_html($keyword->keyword).'</li>';
 					}
 					echo '</ul>';
 				}
@@ -38,7 +38,7 @@ spa_paint_open_tab(SP()->primitives->admin_text('Forum Admin Task Glossary'));
 	spa_paint_open_panel();
 		echo '<div class="helpAndFAQ">';
 			spa_paint_open_fieldset(SP()->primitives->admin_text('Associated Tasks'), false);
-				echo "<div id='codex' class='codex'>".SP()->primitives->admin_text('No current selection from the Task Glossary')."</div>";
+				echo "<div id='codex' class='codex'>".esc_html(SP()->primitives->admin_text('No current selection from the Task Glossary'))."</div>";
 			spa_paint_close_fieldset();
 		echo '</div>';
 	spa_paint_close_panel();
