97e3b8d2b9ded8cabb08a7b42721ce7c823c6e45
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-06-23 11:55:33 +0200

    fix: spa-theme-links.php

--- admin/help/troubleshooting/spa-theme-links.php
+++ admin/help/troubleshooting/spa-theme-links.php
@@ -1,10 +1,10 @@
 <div class="codex">
 
-	<img class="spLeft" src="<?php echo SPCOMMONIMAGES; ?>theme.png" alt="" title="" />
+	<img class="spLeft" src="<?php echo esc_html(SPCOMMONIMAGES); ?>theme.png" alt="" title="" />
 	<div class="codex-head">Simple:Press Themes</div>
 	<div class="clearboth"></div>
 
-	<p>Please visit our <a href="<?php echo(SPHOMESITE); ?>/store/all-premium-themes//">Theme Library</a></p>
+	<p>Please visit our <a href="<?php echo esc_html(SPHOMESITE); ?>/store/all-premium-themes//">Theme Library</a></p>
 	<p>Check out our small but growing library of themes and - in particular - you will find our most recent
 	premium and fully responsive offerings to help your users who prefer to use mobile phones and tablets.</p>
 
