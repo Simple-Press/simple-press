88622b81bac577778aa41ece06b756f68bb6668a
causality_vindgard <joakim.vindgard@causalityagency.com>
2025-06-23 12:24:04 +0200

    fix: spa-support-links.php

--- admin/help/troubleshooting/spa-support-links.php
+++ admin/help/troubleshooting/spa-support-links.php
@@ -1,6 +1,6 @@
 <div class="codex">
 
-	<img class="spLeft" src="<?php echo SPCOMMONIMAGES; ?>member.png" alt="" title="" />
+	<img class="spLeft" src="<?php echo esc_html(SPCOMMONIMAGES); ?>member.png" alt="" title="" />
 	<div class="codex-head">Premium Simple:Press Support</div>
 	<div class="clearboth"></div>
 
@@ -8,7 +8,7 @@
 	<p>Get premium support by purchasing a full Gold or Platinum level bundle of plugins and themes.  These bundles provide full access to a private priority support forums where our user-praised response times will
 	help you get the best out of Simple:Press.</p>
 	<p>Select the Platinum bundle and get priority private ticket support!</p>
-	<p>For pricing and other information please visit <a href="<?php echo SPHOMESITE; ?>/store/add-ons/" >
+	<p>For pricing and other information please visit <a href="<?php echo esc_html(SPHOMESITE); ?>/store/add-ons/" >
 	Simple:Press Pricing Plans</a></p>
 
 </div>
