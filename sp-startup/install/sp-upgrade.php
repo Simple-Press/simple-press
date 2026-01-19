357838089b62a500eaf319c59ad16e92eac54c75
causality_vindgard <joakim.vindgard@causalityagency.com>
2026-01-19 12:02:47 +0100

    chore: update version info

--- sp-startup/install/sp-upgrade.php
+++ sp-startup/install/sp-upgrade.php
@@ -690,6 +690,12 @@ if ($build < $section) {
     sp_response($section);
 }
 
+# Start of Upgrade Routines - 6.11.13 ==========================================================
+$section = 15917;
+if ($build < $section) {
+    sp_response($section);
+}
+
 # ****** IMPORTANT: THE FINAL $section values MUST be the same as the SPBUILD constant
 # ******			for the Upgrade to complete correctly
 
